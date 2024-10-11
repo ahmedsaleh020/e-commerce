259039e8a900070379269936c60aaf1c73e6c562
Ahmed Saleh <ahmed1a1a3a100@gmail.com>
2024-10-11 14:27:31 +0300

    refactor Code

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
-// close and open menu
-const menu = document.querySelector(".links");
-const menuBars = document.querySelector(".menu-icon");
-const menuCloseBtn = document.querySelector(".close-menu");
+// Global Functions
 const openClose = function (element, cls) {
   element.classList.toggle(cls);
 };
-menuBars.addEventListener("click", function () {
-  openClose(menu, "active-menu");
-});
-menuCloseBtn.addEventListener("click", function () {
-  openClose(menu, "active-menu");
-});
+// close and open menu
+function menuManager() {
+  const menu = document.querySelector(".links");
+  const menuBars = document.querySelector(".menu-icon");
+  const menuCloseBtn = document.querySelector(".close-menu");
+  menuBars.addEventListener("click", function () {
+    openClose(menu, "active-menu");
+  });
+  menuCloseBtn.addEventListener("click", function () {
+    openClose(menu, "active-menu");
+  });
+}
+menuManager();
 // close and open cart card
-const cartIcon = document.querySelector(".cart-container");
-const cartCard = document.querySelector(".cart-card");
-const cartCloseBtn = document.querySelector(".close-cart");
-cartIcon.addEventListener("click", function () {
-  openClose(cartCard, "active-cart");
-});
-cartCloseBtn.addEventListener("click", function () {
-  openClose(cartCard, "active-cart");
-});
+function cartCardMAnager() {
+  const cartIcon = document.querySelector(".cart-container");
+  const cartCard = document.querySelector(".cart-card");
+  const cartCloseBtn = document.querySelector(".close-cart");
+  cartIcon.addEventListener("click", function () {
+    openClose(cartCard, "active-cart");
+  });
+  cartCloseBtn.addEventListener("click", function () {
+    openClose(cartCard, "active-cart");
+  });
+}
+cartCardMAnager();
 // Slider
 function slider() {
   // variables
