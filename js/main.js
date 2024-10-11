b6d4c641cdfa8cd71fd76dd1fe2460d1b066ea6f
Ahmed Saleh <ahmed1a1a3a100@gmail.com>
2024-10-11 14:23:01 +0300

    fix the slider bug and refactor the code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-// functions
-const openClose = function (element, cls) {
-  element.classList.toggle(cls);
-};
 // close and open menu
 const menu = document.querySelector(".links");
 const menuBars = document.querySelector(".menu-icon");
 const menuCloseBtn = document.querySelector(".close-menu");
+const openClose = function (element, cls) {
+  element.classList.toggle(cls);
+};
 menuBars.addEventListener("click", function () {
   openClose(menu, "active-menu");
 });
@@ -23,26 +22,77 @@ cartCloseBtn.addEventListener("click", function () {
   openClose(cartCard, "active-cart");
 });
 // Slider
-const slides = document.querySelectorAll(".slide");
-const slider = document.querySelector(".slider");
-const btnLeft = document.querySelector(".slider-btn--left");
-const btnRight = document.querySelector(".slider-btn--right");
-let currentSlide = 0;
-slides.forEach((slide, index) => {
-  slide.style.transform = `translateX(${100 * index}%)`;
-});
+function slider() {
+  // variables
+  const slides = document.querySelectorAll(".slide");
+  const slider = document.querySelector(".slider");
+  const btnLeft = document.querySelector(".slider-btn--left");
+  const btnRight = document.querySelector(".slider-btn--right");
+  const dotContainer = document.querySelector(".dots");
+  let currentSlide = 0;
+  let maxSlide = slides.length - 1;
+  // functions
+  function goToSlide(slideNum) {
+    slides.forEach((slide, index) => {
+      slide.style.transform = `translateX(${100 * (index - slideNum)}%)`;
+    });
+    activateDot(slideNum);
+  }
+  const nextSlide = function () {
+    if (currentSlide === maxSlide) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    goToSlide(currentSlide);
+  };
+  const prevSlide = function () {
+    if (currentSlide === 0) {
+      currentSlide = maxSlide;
+    } else {
+      currentSlide--;
+    }
+    goToSlide(currentSlide);
+  };
 
-btnRight.addEventListener("click", function () {
-  currentSlide++;
-  console.log(currentSlide);
-  slides.forEach((slide, index) => {
-    slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+  const createDots = function () {
+    slides.forEach((_, index) => {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<span class="dot" data-slide="${index}"></span>`
+      );
+    });
+  };
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dot")
+      .forEach((dot) => dot.classList.remove("active"));
+    document
+      .querySelector(`.dot[data-slide="${slide}"`)
+      .classList.add("active");
+  };
+  //event handling
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dot")) {
+      goToSlide(e.target.dataset.slide);
+      e.target.classList.add("active");
+    }
   });
-});
-btnLeft.addEventListener("click", function () {
-  currentSlide--;
-  console.log(currentSlide);
-  slides.forEach((slide, index) => {
-    slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+  document.addEventListener("keydown", function (e) {
+    if (e.key == "ArrowRight") {
+      nextSlide();
+    } else if (e.key == "ArrowLeft") {
+      prevSlide();
+    }
   });
-});
+  createDots();
+  goToSlide(0);
+
+  // slider works automatically
+  setInterval(() => {
+    nextSlide();
+  }, 3000);
+}
+slider();
